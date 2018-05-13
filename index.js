0526172f71eef50d39b15c5d1c2c77b18d6507dc
mjr42 <39211867+mjr42@users.noreply.github.com>
2018-05-13 10:09:09 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+//const botconfig = require("./botconfig.json");
+//const bot = new Discord.Client({disableEveryone: true});
+//bot.on("ready", async () => {
+//  console.log(`${bot.user.username} in online`);
+//});
+
+//bot.login(botconfig.token);
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = 'NDQ0ODAwNjUzMDM1MDQ0ODY1.DdhRtg.EPN_ggOT5Z9-lhAWCuSQWdLrfNs';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('NDQ0ODAwNjUzMDM1MDQ0ODY1.DdhRtg.EPN_ggOT5Z9-lhAWCuSQWdLrfNs');
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+
+client.on('message', message => {
+  if (message.content === 'rhana') {
+    message.channel.send('Goceng sia');
+  }
+});
+
+
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find('name', 'member-log');
+  if (!channel) return;
+  channel.send(`Assalamualaikum wa Mabar teu, ${member}`);
+});
